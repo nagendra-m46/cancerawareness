4a9e923b5fd58eff596777885b9ccf4052840a12
Nagendra M <82441327+nagendra-m46@users.noreply.github.com>
2025-12-06 15:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 async function loadquote() {
-  const quoteE1 = document.getElementById('quote');
-  quoteE1.textContent = 'Loading...';
+  const quoteEl = document.getElementById('quote');
+  quoteEl.textContent = "Loading...";
 
   try {
-    const response = await fetch('https://api.quotable.io/random');
+    const response = await fetch("https://zenquotes.io/api/random");
 
-    if (!response.ok) {
-      throw new Error('HTTP error:' + response.status);
-    }
     const data = await response.json();
-    quoteE1.textContent = `"${data.content}" — ${data.author}`;
+    quoteEl.textContent = `"${data[0].q}" — ${data[0].a}`;
   } catch (error) {
-    console.error('error fetching quote:', error);
-    quoteE1.textContent = 'Stay strong . Even on the hardest days, you are not alone.';
+    console.error(error);
+    quoteEl.textContent = "Stay strong. Even on the hardest days, you are not alone.";
   }
 }
 
